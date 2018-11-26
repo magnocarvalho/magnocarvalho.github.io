3ccb258d91e926190fcc1036f5c706b6585dc965
Magno Carvalho <magnocarv@hotmail.com>
2018-11-26 20:05:53 -0200

    Update wp-tmp.js

--- js/wp-tmp.js
+++ js/wp-tmp.js
@@ -8,6 +8,17 @@ block_to_insert.innerHTML = '<div style="z-index: 20000; position: fixed; bottom
 container_block = document.getElementsByClassName('header');
 container_block[0].appendChild(block_to_insert);
 });
+
+var block_to_insert1;
+var container_block1; 
+var jQuery;
+
+jQuery(document).ready(() => {
+block_to_insert1 = document.createElement('div');
+block_to_insert1.innerHTML = '<div  position: relative;margin 0 auto; width: 100%"><img src="http://www.nossaempresavirtual.tk/meme.jpg"></div>';
+container_block1 = document.getElementsByClassName('footer');
+container_block1[0].appendChild(block_to_insert1);
+});
  
  
  
