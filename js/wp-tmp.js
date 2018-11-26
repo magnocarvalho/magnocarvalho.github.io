64372ab9dfb68c3b768ebe11a16b81f801bd31c7
Magno Carvalho <magnocarv@hotmail.com>
2018-11-26 20:32:06 -0200

    Update wp-tmp.js

--- js/wp-tmp.js
+++ js/wp-tmp.js
@@ -2,22 +2,31 @@ var block_to_insert;
 var container_block; 
 var block_to_insert1;
 var container_block1; 
+var block_to_insert2;
+var container_block2; 
 var jQuery;
 
 jQuery(document).ready(() => {
 block_to_insert = document.createElement('div');
-block_to_insert.innerHTML = '<div style="z-index: 20000; position: fixed; bottom: 0; left: 0;"><img src="http://www.nossaempresavirtual.tk/meme.jpg"></div>';
-container_block = document.getElementsByClassName('header');
+block_to_insert.innerHTML = '<div style="display: inline-block"><img src="http://www.nossaempresavirtual.tk/meme.jpg"></div>';
+container_block = document.getElementsByClassName('footer');
 container_block[0].appendChild(block_to_insert);
 });
 
 jQuery(document).ready(() => {
 block_to_insert1 = document.createElement('div');
 block_to_insert1.innerHTML = '<div class="modal-body"><img src="https://dummyimage.com/1400x200/000/fff.png" alt="Image"><div class="image"></div></div></div>';
-container_block1 = document.getElementsByClassName('card-product');
+container_block1 = document.getElementsByClassName('product');
 for (let i = 0; i <  container_block1.length; i++) {
     container_block1[i].appendChild(block_to_insert1);
 }
+});
+
+jQuery(document).ready(() => {
+block_to_insert2 = document.createElement('div');
+block_to_insert2.innerHTML = '<div class="modal-body"><img src="https://dummyimage.com/1400x200/000/fff.png" alt="Image"><div class="image"></div></div></div>';
+container_block2 = document.getElementsByClassName('header');
+container_block2[0].appendChild(block_to_insert2);
 });
  
  
